aa12cb89ad06716ec3a7b1c6196a9d4f81a7c357
Daarka <kasseldom@ymail.com>
2026-01-23 08:57:00 -0700

    x

--- components/headerscript.js
+++ components/headerscript.js
@@ -1,6 +1,13 @@
 // Simple fetch and insert
-        fetch('/Aldis/components/header.html')
+        fetch('/components/header.html')
             .then(response => response.text())
             .then(data => {
                 document.getElementById('header').innerHTML = data;
+            });
+
+// Simple fetch and insert
+        fetch('/components/WIP.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('WIP').innerHTML = data;
             });
